406a2678b05218f964392de9c11c48d6b682beb7
Salam-Aboul-Hosn <salamaboulhosn581@gmail.com>
2024-06-04 19:13:28 -0700

    Fixed bug, added app to app_module, fix data model

--- backend/job_tracker_backend/api/jobs/models.py
+++ backend/job_tracker_backend/api/jobs/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 
+# Create your models here.
+from django.db import models
+
 
 class Job(models.Model):
     position = models.CharField(max_length=50)
     company = models.CharField(max_length=100)
     date_applied = models.CharField(max_length=50, blank=True, null=True)
-    compensation = models.IntegerField(max_length=50, blank=True, null=True)
+    compensation = models.IntegerField(blank=True, null=True)
     location = models.CharField(max_length=50, blank=True, null=True)
     level = models.CharField(max_length=50, blank=True, null=True)
