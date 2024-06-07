bdd1547c1d75de24f8794e9ef7fa4a7889bfa975
Salam-Aboul-Hosn <salamaboulhosn581@gmail.com>
2024-06-06 22:30:15 -0700

    created urls.py, serializer, DRF

--- backend/job_tracker_backend/api/jobs/models.py
+++ backend/job_tracker_backend/api/jobs/models.py
@@ -1,3 +1,4 @@
+from datetime import timezone
 from django.db import models
 
 # Create your models here.
@@ -7,7 +8,10 @@ from django.db import models
 class Job(models.Model):
     position = models.CharField(max_length=50)
     company = models.CharField(max_length=100)
-    date_applied = models.CharField(max_length=50, blank=True, null=True)
+    date_applied = models.CharField(max_length=50, blank=True, null=True) 
     compensation = models.IntegerField(blank=True, null=True)
     location = models.CharField(max_length=50, blank=True, null=True)
     level = models.CharField(max_length=50, blank=True, null=True)
+
+    def __str__(self):
+            return self.name
--- backend/job_tracker_backend/api/jobs/views.py
+++ backend/job_tracker_backend/api/jobs/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Job
+from .serializers import JobSerializer
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+class JobViewSet(viewsets.ModelViewSet):
+    queryset = Job.objects.all()
+    serializer_class = JobSerializer
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
