8b1cc6e0951130c6f7c11157a624b8dcfe140229
Salam-Aboul-Hosn <salamaboulhosn581@gmail.com>
2024-06-21 23:25:33 -0700

    Setting up cors

--- backend/job_tracker_backend/api/jobs/models.py
+++ backend/job_tracker_backend/api/jobs/models.py
@@ -8,7 +8,7 @@ class Job(models.Model):
     position = models.CharField(max_length=50)
     company = models.CharField(max_length=100)
     date_applied = models.CharField(max_length=50, blank=True, null=True) 
-    compensation = models.IntegerField(blank=True, null=True)
+    compensation = models.CharField(max_length=50, blank=True, null=True)
     location = models.CharField(max_length=50, blank=True, null=True)
     level = models.CharField(max_length=50, blank=True, null=True)
 
