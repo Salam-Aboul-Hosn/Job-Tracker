a0b06b7b7dbe9dea4fa16c438ee404f2dde34f38
Salam-Aboul-Hosn <salamaboulhosn581@gmail.com>
2024-06-29 12:52:49 -0700

    Setting up a temporary docker file

--- backend/job_tracker_backend/config/urls.py
+++ backend/job_tracker_backend/config/urls.py
@@ -19,6 +19,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('', root_view),
     path('api/', include('job_tracker_backend.api.jobs.urls'))
 ]
